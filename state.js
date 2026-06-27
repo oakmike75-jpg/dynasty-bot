e99deaa604f5030e411e18473034f147abd380ea
oakmike75-jpg <oakmike75@gmail.com>
2026-06-26 21:36:54 -0400

    Update state.js

--- state.js
+++ state.js
@@ -1,6 +1,39 @@
-const fs=require('fs'),path=require('path'),F=path.join(__dirname,'state.json');
-let _s={};
-if(fs.existsSync(F)){try{_s=JSON.parse(fs.readFileSync(F,'utf8'));console.log('📂 State loaded')}catch{_s={}}}
-const K=['channelsCreated','seenTransactions','lastChatId','rosterSnapshot','nflWeek','powerRankLastWeek','standingsMessageIds','rosterChannelMap'];
-function save(){const o={};K.forEach(k=>{if(_s[k]!==undefined)o[k]=_s[k]});fs.writeFileSync(F,JSON.stringify(o,null,2))}
-module.exports={get:k=>_s[k],set:(k,v)=>{_s[k]=v;save()}};
+const fs = require('fs');
+const path = require('path');
+const STATE_FILE = path.join(__dirname, 'state.json');
+
+let _state = {};
+
+if (fs.existsSync(STATE_FILE)) {
+  try {
+    _state = JSON.parse(fs.readFileSync(STATE_FILE, 'utf8'));
+    console.log('📂 State loaded');
+  } catch {
+    _state = {};
+  }
+}
+
+const PERSIST_KEYS = [
+  'channelsCreated', 'seenTransactions', 'rosterSnapshot',
+  'nflWeek', 'rosterChannelMap', 'contentPosted'
+];
+
+function save() {
+  const out = {};
+  PERSIST_KEYS.forEach(k => {
+    if (_state[k] !== undefined) out[k] = _state[k];
+  });
+  try {
+    fs.writeFileSync(STATE_FILE, JSON.stringify(out, null, 2));
+  } catch (e) {
+    console.log('Could not save state:', e.message);
+  }
+}
+
+module.exports = {
+  get: (key) => _state[key],
+  set: (key, value) => {
+    _state[key] = value;
+    save();
+  }
+};
